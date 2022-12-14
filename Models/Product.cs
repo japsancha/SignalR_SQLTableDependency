bc64ae6baad98adfd72eb08000911fe773af42a9
Joaquim Sancha <japsancha@users.noreply.github.com>
2022-12-14 13:28:52 -0100

    Change the model product

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,29 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-
 namespace SignalR_SQLTableDependency.Models
 {
 	public class Product
 	{
-		property int Id
+		public int Id
 		{
 			get;
 			set;
 		}
-		property string? Name
+		public string? Name
 		{
 			get;
 			set;
 		}
-		property string? Category
+		public string? Category
 		{
 			get;
 			set;
 		}
-		property decimal Price
+		public decimal Price
 		{
 			get;
 			set;
